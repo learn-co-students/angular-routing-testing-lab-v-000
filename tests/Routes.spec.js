7184abe1d36e6e46f00b3b34f36fe60d8299a0c0
jakebrady5 <jakebrady5@gmail.com>
2016-03-27 16:43:50 -0400

    Done.

---
+++ tests/Routes.spec.js
@@ -0,0 +1,56 @@
+describe('Routes', function(){
+  var $state;
+  beforeEach(module('app'));
+  beforeEach(inject(function($injector){
+    $state = $injector.get('$state');
+  }));
+
+  describe('Settings', function(){
+    var state;
+    it('should have the correct URL', function(){
+      state = $state.get('settings');
+      expect(state.url).toEqual('/settings');
+    });
+
+    it('should have the correct templateURL', function(){
+      expect(state.templateUrl).toEqual('views/settings.html');
+    });
+
+    it('should have the correct controller', function(){
+      expect(state.controller).toEqual('SettingsController');
+    });
+  });
+
+  describe('settings.user', function(){
+    var state;
+    it('should have the correct URL', function(){
+      state = $state.get('settings.user');
+      expect(state.url).toEqual('/user');
+    });
+
+    it('should have the correct templateURL', function(){
+      expect(state.templateUrl).toEqual('views/settings/user.html');
+    });
+
+    it('should have the correct controller', function(){
+      expect(state.controller).toEqual('UserSettingsController');
+    });
+  });
+
+  describe('Settings', function(){
+    var state;
+    it('should have the correct URL', function(){
+      state = $state.get('settings.notifications');
+      expect(state.url).toEqual('/notifications');
+    });
+
+    it('should have the correct templateURL', function(){
+      expect(state.templateUrl).toEqual('views/settings/notifications.html');
+    });
+
+    it('should have the correct controller', function(){
+      expect(state.controller).toEqual('NotificationsSettingsController');
+    });
+  });
+
+});
