3cbf8bc9f5c641c80db6e579663ee2c38c2d8436
Scotty Runyan <runyans7@gmail.com>
2016-03-28 15:06:17 -0400

    Done.

---
+++ tests/Routes.spec.js
@@ -0,0 +1,18 @@
+describe('Routes', function() {
+  var state;
+
+  beforeEach(module('app'));
+
+  beforeEach(inject(function($injector) {
+    $state = $injector.get('$state');
+  }));
+
+  describe('Settings', function() {
+    var state;
+
+    it('Should have the correct URL', function() {
+      state = $state.get('settings');
+      expect(state.url).toEqual('/settings');
+    })
+  })
+})
