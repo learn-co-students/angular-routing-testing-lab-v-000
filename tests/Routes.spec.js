6cd34b1a967aa573543495aee2a73b728d3a3471
Adam King <adam.king0126@gmail.com>
2016-04-13 19:03:31 -0500

    Done.

---
+++ tests/Routes.spec.js
@@ -0,0 +1,53 @@
+describe ('Routes', function () {
+    var $state;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function ($injector) {
+      $state = $injector.get('$state');
+    }));
+
+  describe('settings', function () {
+     var state; 
+
+     it('should have the correct URL', function () {
+       state = $state.get('settings');
+       
+       expect(state.url).toEqual('/settings');
+     });
+
+     it('should use the correct template', function () {
+       expect(state.templateUrl).toEqual('views/settings.html');
+     });
+  });
+
+
+  describe('settings.user', function () {
+     var state; 
+
+     it('should have the correct URL', function () {
+       state = $state.get('settings.user');
+       
+       expect(state.url).toEqual('/user');
+     });
+
+     it('should use the correct template', function () {
+       expect(state.templateUrl).toEqual('views/settings/user.html');
+     });
+  });
+
+
+  describe('settings.notifications', function () {
+     var state; 
+
+     it('should have the correct URL', function () {
+       state = $state.get('settings.notifications');
+       
+       expect(state.url).toEqual('/notifications');
+     });
+
+     it('should use the correct template', function () {
+       expect(state.templateUrl).toEqual('views/settings/notifications.html');
+     });
+  });
+});
