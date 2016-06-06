153c13412987327c0638ef1c05abbb651f833541
fiveinfinity <jlachance1@gmail.com>
2016-06-06 15:56:26 -0500

    added 6 Karma route tests

---
+++ tests/Routes.spec.js
@@ -0,0 +1,47 @@
+describe('Routes', function() {
+  var $state;
+
+  beforeEach(module('app'));
+  beforeEach(inject(function($injector) {
+    $state = $injector.get('$state');
+  }));
+
+  describe('Settings', function() {
+    var state;
+
+    it('should have the correct template', function() {
+      state = $state.get('settings');
+      expect(state.templateUrl).toEqual('views/settings.html');
+    });
+
+    it('should use the correct Controller', function() {
+      expect(state.controller).toEqual('SettingsController');
+    });
+  });
+
+  describe('User Settings', function() {
+    var state;
+
+    it('should have the correct template', function() {
+      state = $state.get('settings.user');
+      expect(state.templateUrl).toEqual('views/settings/user.html');
+    });
+
+    it('should use the correct Controller', function() {
+      expect(state.controller).toEqual('UserSettingsController');
+    });
+  });
+
+  describe('Notifications', function() {
+    var state;
+
+    it('should have the correct template', function() {
+      state = $state.get('settings.notifications');
+      expect(state.templateUrl).toEqual('views/settings/notifications.html');
+    });
+
+    it('should use the correct Controller', function() {
+      expect(state.controller).toEqual('NotificationsSettingsController');
+    });
+  });
+});
