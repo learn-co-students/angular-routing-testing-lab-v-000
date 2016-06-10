17aaaebac8d0b2299406b93d4fd3191d0b76787d
viparthasarathy <viparthasarathy@gmail.com>
2016-06-10 15:03:49 -0700

    Done.

---
+++ tests/Routes.spec.js
@@ -0,0 +1,48 @@
+describe('Routes', function() {
+  var $state;
+
+  beforeEach(module('app'));
+
+  beforeEach(inject(function($injector) {
+    $state = $injector.get('$state');
+  }));
+
+  describe('Settings', function() {
+    var state;
+
+    it('should use the correct template', function() {
+      state = $state.get('settings');
+      expect(state.templateUrl).toEqual('views/settings.html');
+    });
+
+    it('should use the correct controller', function() {
+      expect(state.controller).toEqual('SettingsController');
+    });
+  });
+
+  describe('User Settings', function() {
+    var state;
+
+    it('should use the correct template', function() {
+      state = $state.get('settings.user');
+      expect(state.templateUrl).toEqual('views/settings/user.html');
+    });
+
+    it('should use the correct controller', function() {
+      expect(state.controller).toEqual('UserSettingsController');
+    });
+  });
+
+  describe('Notifications Settings', function() {
+    var state;
+
+    it('should use the correct template', function() {
+      state = $state.get('settings.notifications');
+      expect(state.templateUrl).toEqual('views/settings/notifications.html');
+    });
+
+    it('should use the correct controller', function() {
+      expect(state.controller).toEqual('NotificationsSettingsController');
+    });
+  });
+})
